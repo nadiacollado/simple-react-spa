4ac6ada2f68834fb72f43166c0987154ef1d4624
Nadia Collado <ncollado@8thlight.com>
2025-04-03 13:57:42 -0400

    Extract ArtifactList, clean up code

---
+++ my-app/src/components/ArtifactList.tsx
@@ -0,0 +1,41 @@
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
+import { fetchArtifacts } from "../services/metMuseumService";
+import { Artifact } from "../types/artifact";
+
+const ArtifactList = () => {
+  const {
+    data: artifacts,
+    isLoading,
+    error,
+  } = useQuery<Artifact[]>({
+    queryKey: ["artifacts"],
+    queryFn: fetchArtifacts,
+    staleTime: Infinity,
+  });
+
+  if (isLoading) return <p>Loading Met Museum artifacts...</p>;
+  if (error || !artifacts)
+    return <p>There’s been an issue loading artifacts. Please contact Nadia.</p>;
+
+  return (
+    <div>
+      <h1>Met Museum Artifacts</h1>
+      <div className="grid">
+        {artifacts.map((artifact) => (
+          <Link
+            to={`/artifact/${artifact.objectID}`}
+            key={artifact.objectID}
+            className="card"
+          >
+            <img src={artifact.primaryImageSmall} alt={artifact.title} />
+            <h2>{artifact.title}</h2>
+            <p>{artifact.artistDisplayName || "Unknown Artist"}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ArtifactList;
--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./pages/App/App.tsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -11,9 +11,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-\        </Routes>
+        <App />
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>
